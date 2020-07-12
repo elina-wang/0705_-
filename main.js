642be4c609dd09a8dab23b699541703dabb8d404
elina-wang <siaosyuan0611@gmail.com>
2020-07-12 14:24:46 +0800

    設定完成

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 window.onload = () => {
     console.log("頁面載入完成")
 
-    var test = document.getElementById("test");
+    // 取得所有元素
+    var elements = document.getElementsByTagName("*");
+    
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
 
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target");  //目標屬性
+            var offset = e.getAttribute("data-gte-offset");  //位移屬性
+    
+            if (target) {
+                var element = document.getElementById(target);  //取得目標
+    
+                // 視窗.捲動至({元素.上方,行為})
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior: "smooth"
+                })
+    }
 
-        console.log(target);
-        console.log(offset);
+        }
+
+
 
     }
 }
